32021426f2a398f6e37279175e2280d27dca57e8
AlexKonopada <6761240@gmail.com>
2022-10-22 02:00:02 +0300

    fixed some more bugs

--- src/main/java/flower/store/FlowerBucket.java
+++ src/main/java/flower/store/FlowerBucket.java
@@ -5,10 +5,12 @@ import java.util.ArrayList;
 public class FlowerBucket {
     private ArrayList<FlowerPack> bucketOfFlowers;
     private double bucketPrice;
-    public FlowerBucket(ArrayList<FlowerPack> packOfFlowers) { // when we already have a pack
+    public FlowerBucket(ArrayList<FlowerPack> packOfFlowers) {
+        // when we already have a pack
         this.bucketOfFlowers = packOfFlowers;
     }
-    public FlowerBucket() { // empty flower bucket
+    public FlowerBucket() {
+        // empty flower bucket
         this.bucketOfFlowers = new ArrayList<FlowerPack>();
     }
     public void add(FlowerPack flowerPack) {
--- src/main/java/flower/store/Store.java
+++ src/main/java/flower/store/Store.java
@@ -34,39 +34,42 @@ public class Store {
         flowerBucketList.add(flowerBucket);
     }
 
-    public ArrayList<Flower>
-    makeFlowerPurchase (int minPrice, int maxPrice) {
+    public ArrayList<Flower> makeFlowerPurchase (
+            int minPrice, int maxPrice) {
         if (!this.flowerList.isEmpty()) {
             this.flowerList.clear();
         }
         for (Flower flower: flowerList) {
-            if (flower.getPrice() >= minPrice && flower.getPrice() <= maxPrice) {
+            if (flower.getPrice() >= minPrice &&
+                    flower.getPrice() <= maxPrice) {
                 this.flowerList.add(flower);
             }
         }
         return this.flowerList;
     }
 
-    public ArrayList<FlowerPack>
-    makeFlowerPackPurchase (int minPrice, int maxPrice) {
+    public ArrayList<FlowerPack> makeFlowerPackPurchase (
+            int minPrice, int maxPrice) {
         if (!this.flowerPackList.isEmpty()) {
             this.flowerPackList.clear();
         }
         for (FlowerPack flowerPack: flowerPackList) {
-            if (flowerPack.getPrice() >= minPrice && flowerPack.getPrice() <= maxPrice) {
+            if (flowerPack.getPrice() >= minPrice &&
+                    flowerPack.getPrice() <= maxPrice) {
                 this.flowerPackList.add(flowerPack);
             }
         }
         return this.flowerPackList;
     }
 
-    public ArrayList<FlowerBucket>
-    makeFlowerBucketPurchase(int minPrice, int maxPrice) {
+    public ArrayList<FlowerBucket> makeFlowerBucketPurchase(
+            int minPrice, int maxPrice) {
         if (!this.flowerBucketList.isEmpty()) {
             this.flowerBucketList.clear();
         }
         for (FlowerBucket flowerBucket: flowerBucketList) {
-            if (flowerBucket.getPrice() >= minPrice && flowerBucket.getPrice() <= maxPrice) {
+            if (flowerBucket.getPrice() >= minPrice &&
+                    flowerBucket.getPrice() <= maxPrice) {
                 this.flowerBucketList.add(flowerBucket);
             }
         }
